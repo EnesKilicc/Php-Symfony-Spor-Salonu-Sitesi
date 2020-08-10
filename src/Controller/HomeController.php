b42bbecdf7bc7dec92e1c538b2b1a45550f84d7e
EnesKilicc <enesklcc97@gmail.com>
2020-08-11 00:39:08 +0300

    email gonderme

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,8 +8,12 @@ use App\Repository\SettingRepository;
 use App\Repository\SporPaketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\Bridge\Google\Transport\GmailSmtpTransport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 
 class HomeController extends AbstractController
 {
@@ -19,7 +23,7 @@ class HomeController extends AbstractController
     public function index(SettingRepository $settingRepository,SporPaketRepository $paketRepository)
     {
         $setting = $settingRepository->findAll();
-        $slider = $paketRepository->findAll();
+        $slider = $paketRepository->findBy([],['title'=>'ASC'],4);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'setting' => $setting,
@@ -46,7 +50,7 @@ class HomeController extends AbstractController
         $form = $this->createForm(MessagesType::class, $message);
         $form->handleRequest($request);
         $submittedToken = $request->request->get('token');
-
+        $setting = $settingRepository->findAll();
 
         if ($form->isSubmitted()) {
             if($this->isCsrfTokenValid('form-message',$submittedToken)){
@@ -56,12 +60,27 @@ class HomeController extends AbstractController
             $entityManager->persist($message);
             $entityManager->flush();
             $this->addFlash('success','Mesajınız başarıyla gonderildi');
+                $email = (new Email())
+                    ->from($setting[0]->getSmtpEmail())
+                    ->to($form['email']->getData())
+                    //->cc('cc@example.com')
+                    //->bcc('bcc@example.com')
+                    //->replyTo('fabien@example.com')
+                    //->priority(Email::PRIORITY_HIGH)
+                    ->subject('Time for Symfony Mailer!')
+                    //->text('Sending emails is fun again!')
+                    ->html("Dear ".$form['name']->getData()."<br>
+                        <p> bu mesaj spor salonumuzdan gonderildi</p>"
+                    );
+                $transport = new GmailSmtpTransport($setting[0]->getSmtpemail(),$setting[0]->getSmtppassword());
+                $mailer = new Mailer($transport);
+                $mailer->send($email);
 
             return $this->redirectToRoute('home_contact');
             }
 
         }
-        $setting = $settingRepository->findAll();
+
         return $this->render('home/contact.html.twig', [
             'setting' =>$setting,
             'form' => $form->createView(),
